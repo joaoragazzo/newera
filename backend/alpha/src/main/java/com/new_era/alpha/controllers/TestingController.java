a556cbc7387c0b02301e05574c9669544c621810
joaoragazzo <joao_ragazzo@hotmail.com>
2024-12-28 03:46:22 -0300

    feat: removing useless dependencies

--- backend/alpha/src/main/java/com/new_era/alpha/controllers/TestingController.java
+++ backend/alpha/src/main/java/com/new_era/alpha/controllers/TestingController.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.print.DocFlavor.STRING;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.new_era.alpha.entities.player.Nick;
