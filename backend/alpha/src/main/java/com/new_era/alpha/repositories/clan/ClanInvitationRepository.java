89bd256b2d433a0e48dff011beb087ffef3c49ee
joaoragazzo <joao_ragazzo@hotmail.com>
2025-01-04 01:26:22 -0300

    fix: fix invalid invite requests

--- backend/alpha/src/main/java/com/new_era/alpha/repositories/clan/ClanInvitationRepository.java
+++ backend/alpha/src/main/java/com/new_era/alpha/repositories/clan/ClanInvitationRepository.java
@@ -12,7 +12,7 @@ import com.new_era.alpha.entities.clan.ClanInvitation;
 @Repository
 public interface ClanInvitationRepository extends JpaRepository<ClanInvitation, Integer> {
     
-    @Query("SELECT ci FROM ClanInvitation ci WHERE ci.expire_at > CURRENT_TIMESTAMP AND ci.player.id = :playerId AND ci.clan.id = :clanId")
+    @Query("SELECT ci FROM ClanInvitation ci WHERE ci.expire_at > CURRENT_TIMESTAMP AND ci.player.id = :playerId AND ci.clan.id = :clanId AND ci.accepted_at IS NULL AND ci.declined_at IS NULL")
     List<ClanInvitation> findAllActiveInvitesFromAClanToPlayer(@Param("playerId") Integer playerId, @Param("clanId") Integer clanId);
 
 }
