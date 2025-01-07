6106d8ca5c3836649e60e04ec8f223bf7568ce60
joaoragazzo <joao_ragazzo@hotmail.com>
2025-01-07 06:54:47 -0300

    refact: better performance for database insertions

--- debug/create_sql_file.py
+++ debug/create_sql_file.py
@@ -31,52 +31,63 @@ def generate_steam64id():
 CLANS = []
 PLAYERS_ON_CLAN = []
 
-with open('database.sql', 'w') as sql_file:
+players = []
+nicks = []
+clans = []
+clan_filiations = []
+
+for user_id in range(1, 9999):
+    karma = randint(0, 1000)
+    steam64id = generate_steam64id()
     
-    for user_id in range(1, 9999):
-        karma = randint(0, 1000)
-        steam64id = generate_steam64id()
-        
-        has_discord_id = True if randint(0, 100) > 60 else False
+    has_discord_id = True if randint(0, 100) > 60 else False
 
-        if has_discord_id:
-            discord_id = f"\"{randint(100000000000000000, 999999999999999999)}\""
-        else:
-            discord_id = "NULL"
+    if has_discord_id:
+        discord_id = f"{randint(100000000000000000, 999999999999999999)}"
+    else:
+        discord_id = None
 
-        user_add_sql = f"INSERT INTO player (steam64id, discord_id, karma) VALUES (\"{steam64id}\", {discord_id}, {karma});\n"
-        sql_file.write(user_add_sql)
+    players.append(f"(\"{steam64id}\", {f'\"{discord_id}\"' if discord_id else 'NULL'}, {karma})")
 
-        amount_of_nicks = randint(1, 3)
+    amount_of_nicks = randint(1, 3)
 
-        for _ in range(0, amount_of_nicks):
-            nickname = faker.first_name_male()
-            formatted_datetime, _ = generate_random_localdatetime()
-            nickname_add_sql = f"INSERT INTO nick (name, player_id, created_at) VALUES (\"{nickname}\", {user_id}, \"{formatted_datetime}\");\n"
-            sql_file.write(nickname_add_sql)
+    for _ in range(0, amount_of_nicks):
+        nickname = faker.first_name_male()
+        formatted_datetime, _ = generate_random_localdatetime()
+        nicks.append(f"(\"{nickname}\", {user_id}, \"{formatted_datetime}\")")
 
-        leader_of_clan = True if randint(0, 100) > 90 else False 
+    leader_of_clan = True if randint(0, 100) > 90 else False 
 
-        if leader_of_clan:
-            clan_id = len(CLANS) + 1
-            clan_name = faker.cryptocurrency_name()
-            clan_tag = faker.cryptocurrency_code()[:3]
-            created_at, datetime_ = generate_random_localdatetime()
-            color = faker.hex_color()
+    if leader_of_clan:
+        clan_id = len(CLANS) + 1
+        clan_name = faker.cryptocurrency_name()
+        clan_tag = faker.cryptocurrency_code()[:3]
+        created_at, datetime_ = generate_random_localdatetime()
+        color = faker.hex_color()
 
-            clan_add_sql = f"INSERT INTO clan (name, tag, color, created_at) VALUES (\"{clan_name}\", \"{clan_tag}\", \"{color}\", \"{created_at}\");\n"
-            sql_file.write(clan_add_sql)
+        clans.append(f"(\"{clan_name}\", \"{clan_tag}\", \"{color}\", \"{created_at}\")")
 
-            clan_filiation_sql = f"INSERT INTO clan_filiation (player_id, clan_id, role, joined_at) VALUES ({user_id}, {clan_id}, \"OWNER\", \"{created_at}\");\n"
-            sql_file.write(clan_filiation_sql)
-            CLANS.append(clan_id)
-        
-            for player in range(1, user_id):
-                player_in_clan = True if randint(0, 100) > 98 else False
+        clan_filiations.append(f"({user_id}, {clan_id}, \"OWNER\", \"{created_at}\")")
+        CLANS.append(clan_id)
+    
+        for player in range(1, user_id):
+            player_in_clan = True if randint(0, 100) > 98 else False
 
-                if player_in_clan and player not in PLAYERS_ON_CLAN:
-                    member_role = "ADMIN" if randint(0, 100) > 75 else "MEMBER"
-                    joined_at, _ = generate_random_localdatetime(min_date=datetime_)
-                    clan_filiation_sql = f"INSERT INTO clan_filiation (player_id, clan_id, role, joined_at) VALUES ({player}, {clan_id}, \"{member_role}\", \"{joined_at}\");\n"
-                    sql_file.write(clan_filiation_sql)
-                    PLAYERS_ON_CLAN.append(player)
+            if player_in_clan and player not in PLAYERS_ON_CLAN:
+                member_role = "ADMIN" if randint(0, 100) > 75 else "MEMBER"
+                joined_at, _ = generate_random_localdatetime(min_date=datetime_)
+                clan_filiations.append(f"({player}, {clan_id}, \"{member_role}\", \"{joined_at}\")")
+                PLAYERS_ON_CLAN.append(player)
+
+with open('database.sql', 'w') as sql_file:
+    if players:
+        sql_file.write(f"INSERT INTO player (steam64id, discord_id, karma) VALUES {', '.join(players)};\n")
+
+    if nicks:
+        sql_file.write(f"INSERT INTO nick (name, player_id, created_at) VALUES {', '.join(nicks)};\n")
+
+    if clans:
+        sql_file.write(f"INSERT INTO clan (name, tag, color, created_at) VALUES {', '.join(clans)};\n")
+
+    if clan_filiations:
+        sql_file.write(f"INSERT INTO clan_filiation (player_id, clan_id, role, joined_at) VALUES {', '.join(clan_filiations)};\n")
